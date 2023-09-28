b2419aebd43106e8decb78566a96b05b1dafa70d
torgav <christian.torresgaviria@elev.ga.ntig.se>
2023-09-28 14:44:37 +0200

    Har skapat up UIn för spelet och alla andra design val. Hr agnet färger och directioner för snaken. Har aväen inmplementerat en inmatning checkare för att kolla vad användaren tar in. Dessutom med det en updaterare för skrämen när ändringar på går.

--- snakeGame.py
+++ snakeGame.py
@@ -3,6 +3,7 @@ import random
 from enum import Enum
 from collections import namedtuple
 pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
 
 #Direktionerna som är tillgänglig i spelet.
 class Direction(Enum):
@@ -11,10 +12,17 @@ class Direction(Enum):
     UP = 3
     DOWN = 4
 
-
-Point = namedtuple('Point', 'x', 'y')
+Point = namedtuple('Point','x, y')
 
 BLOCK_SIZE = 20
+SPEED = 40
+
+#Färger
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+RED = (200,0,0)
+BLUE1 = (0,0,255)
+BLUE2 = (0,100,255)
 
 class SnakeGame:
     def __init__(self, w=640, h=480):
@@ -57,18 +65,69 @@ class SnakeGame:
         self.food = Point(x, y)
         #Vi måste kolla om maten har kanske kommit fram på ormen, då behövs det att vi gör om det.
         if self.food in self.snake:
-            self._place_foodfood()
+            self._place_food()
 
     def play_step(self):
-        pass
+        #a. få in inputs
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.direction = Direction.LEFT    
+                elif event.key == pygame.K_RIGHT:
+                    self.direction = Direction.RIGHT
+                elif event.key == pygame.K_UP:
+                    self.direction = Direction.UP
+                if event.key == pygame.K_DOWN:
+                    self.direction = Direction.DOWN
+
+        #b. Ormen rör sig
+        self._move(self.direction) #updatterar huvudet av ormen
+        self.snake.insert(0, self.head)
+
+        #c. Kolla om ormen har dött = spelet är slut
+
+        #d. Placera nytt frukt eller röra sig 
+
+        #e. Updatera interna klockan och skörmen.¨
+        self._update_ui()
+        self.clock.tick(SPEED)
+
+        #f. returnera spelet är över och poäng
+
+        game_over = False
+        return game_over, self.score
+
+    def _update_ui(self):
+        self.display.fill(BLACK)
+
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE-2, BLOCK_SIZE-2))
+
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
+
+
+    def _move(self, direction):
+        x = self.head.x
+        y = self.head.y
 
 if __name__== '__main__':
     game = SnakeGame()
 
     #Spel loopen
     while True:
-        game.play_step()
+        game_over, score = game.play_step()
 
+        if game_over == True :
+            break
 
+    print("Score:", score)
 
     pygame.quit() 
